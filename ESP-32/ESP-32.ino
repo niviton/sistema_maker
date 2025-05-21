e5d9d4e95e515027480744493f985da5a8ee7f4f
niviton <niviton.edu@gmail.com>
2025-05-21 18:24:56 -0300

    Minhas alterações locais

---
+++ ESP-32/ESP-32.ino
@@ -0,0 +1,37 @@
+// serial_relay.ino
+
+const int RELAY_PIN = 17;   // seu pino de relé
+const int LED_PIN   = 2;    // GPIO2, LED embutido em muitos módulos DevKit
+
+void setup() {
+  pinMode(RELAY_PIN, OUTPUT);
+  digitalWrite(RELAY_PIN, LOW);
+
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW);
+
+  Serial.begin(115200);
+}
+
+void loop() {
+  if (Serial.available()) {
+    String cmd = Serial.readStringUntil('\n');
+    cmd.trim();
+
+    if (cmd.equalsIgnoreCase("OPEN")) {
+      // Pulso no relé
+      digitalWrite(RELAY_PIN, HIGH);
+      delay(100);
+      digitalWrite(RELAY_PIN, LOW);
+      Serial.println("RELAY_OPENED");
+
+    } else if (cmd.equalsIgnoreCase("LED_ON")) {
+      digitalWrite(LED_PIN, HIGH);
+      Serial.println("LED_ON_OK");
+
+    } else if (cmd.equalsIgnoreCase("LED_OFF")) {
+      digitalWrite(LED_PIN, LOW);
+      Serial.println("LED_OFF_OK");
+    }
+  }
+}
